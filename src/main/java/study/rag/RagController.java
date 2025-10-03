54861347033e4feea1906f36fa90b10937d87511
George Zhou <pengcheng.zhou@gmail.com>
2025-10-03 02:45:41 -0400

    Turn on spring ai request/response log

--- src/main/java/study/rag/RagController.java
+++ src/main/java/study/rag/RagController.java
@@ -1,6 +1,7 @@
 package study.rag;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,18 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RagController {
     private final ChatClient chatClient;
+    private final SimpleLoggerAdvisor loggerAdvisor;
 
-    public RagController(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
+    public RagController(ChatClient.Builder chatClientBuilder, VectorStore vectorStore, SimpleLoggerAdvisor loggerAdvisor) {
         this.chatClient = chatClientBuilder
                 .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
                 .build();
+        this.loggerAdvisor = loggerAdvisor;
     }
 
     @GetMapping("/rag")
-    public Models rag(@RequestParam(defaultValue = "Give me a list of all the models from OpenAI and their context window size") String message) {
-        return chatClient.prompt()
-                .user(message)
-                .call()
-                .entity(Models.class);
+    public Models rag(@RequestParam(defaultValue = "Give me a list of all the models from OpenAI and their context window size") String message
+            , @RequestParam(defaultValue = "false") boolean debug) {
+        ChatClient.ChatClientRequestSpec reqSpec = chatClient.prompt()
+                .user(message);
+        if (debug) {
+            reqSpec.advisors(loggerAdvisor);
+        }
+        return reqSpec.call()
+            .entity(Models.class);
     }
 }
